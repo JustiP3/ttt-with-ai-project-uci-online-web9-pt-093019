684c2a35104559794be4da5a0167b65316b34708
Justin Phillips <justinphil02@yahoo.com>
2019-12-08 18:29:11 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,11 +1,18 @@
 module Players
   class Computer < Player
     def move(board_instance)
-      position = rand(9)
-      until(board_instance.valid_move?(position))
-        position = rand(10)
-      end
-      position.to_s
+      offence_defence = "0" #0 = offence, 1 = defence 
+      
+      if board_instance.turn_count == 0 
+        position = "1"
+      end 
+  #    position = rand(9)
+  #    until(board_instance.valid_move?(position))
+  #      position = rand(10)
+  #    end
+  #    position.to_s
     end
   end
 end
+
+

f5bab2dd1d3878effce21de9b26764bea5881565
Justin Phillips <justinphil02@yahoo.com>
2019-12-08 18:51:51 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,8 +3,14 @@ module Players
     def move(board_instance)
       corners = [1,3,7,9]
       offence_defence = "0" #0 = offence, 1 = defence
-      binding.pry
-      a = Game::WIN_COMBINATIONS
+      opponent_win_combo = []
+      
+      Game::WIN_COMBINATIONS.each do |combo|
+          temp_combo = combo.select {|index| board_instance.cells[index] != self.token && board_instance.cells[index] != " "}
+          if temp_combo.size == 2 
+            opponent_win_combo = temp_combo
+          end 
+      end 
 
       if board_instance.turn_count == 0
         position = corners[rand(4)].to_s

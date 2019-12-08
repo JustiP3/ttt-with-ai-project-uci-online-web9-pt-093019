33f5e490b7a766c5f06798329ab8011b2b9a6308
Justin Phillips <justinphil02@yahoo.com>
2019-12-08 18:59:30 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,47 +2,47 @@ module Players
   class Computer < Player
 
     def initialize
-      super 
+      super
       @opponent_win_combo = []
       @corners = [1,3,7,9]
-    end 
+    end
 
-    attr_accessor :opponent_win_combo 
-    attr_reader :corners 
-    
+    attr_accessor :opponent_win_combo
+    attr_reader :corners
+
     def move(board_instance)
-      
-      
 
 
 
-      if offence_defence == "0" # offence 
+
+
+      if offence_defence == "0" # offence
         if board_instance.turn_count == 0
           position = corners[rand(4)].to_s
         elsif board_instance.turn_count == 1
           position = "5"
         end
-      elsif offence_defence == "1" # defence 
+      elsif offence_defence == "1" # defence
 
-      end 
+      end
   #    position = rand(9)
   #    until(board_instance.valid_move?(position))
   #      position = rand(10)
   #    end
   #    position.to_s
     end
-    
-    def offence_defence #0 = offence, 1 = defence 
+
+    def offence_defence #0 = offence, 1 = defence
       offence_defence = "0"
       Game::WIN_COMBINATIONS.each do |combo|
           temp_combo = combo.select {|index| board_instance.cells[index] != self.token && board_instance.cells[index] != " "}
           if temp_combo.size == 2
             opponent_win_combo = temp_combo
-            offence_defence = "1" 
+            offence_defence = "1"
           end
       end
       offence_defence
-    end 
-    
-  end #class 
-end #module 
+    end
+
+  end #class
+end #module

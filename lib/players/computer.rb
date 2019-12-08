57df07d3d6deff0940e69149c69f9d1c309d18f4
Justin Phillips <justinphil02@yahoo.com>
2019-12-08 18:41:53 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,10 +1,15 @@
 module Players
   class Computer < Player
     def move(board_instance)
+      corners = [1,3,7,9]
       offence_defence = "0" #0 = offence, 1 = defence
+      
+      A = Game.WIN_COMBINATIONS
 
       if board_instance.turn_count == 0
-        position = "1"
+        position = corners[rand(0,4)].to_s
+      elsif board_instance.turn_count == 1
+        position = "5"
       end
   #    position = rand(9)
   #    until(board_instance.valid_move?(position))
